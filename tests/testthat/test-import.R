bd606c7608b81910f53fa0d2f2fc50365c8af5b6
jeffersonfparil <jeffersonparil@gmail.com>
2024-07-31 10:49:32 +1000

    still working on the documentation

--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -17,3 +17,25 @@ test_that("fn_check_import_inputs", {
     expect_equal(is.null(null_error), TRUE)
     expect_equal(methods::is(non_null_error, "dbError"), TRUE)
 })
+
+test_that("fn_remove_quotes_and_newline_characters_in_data", {
+    set.seed(123)
+    list_fnames_tables = fn_simulate_tables(
+         n_entries=50,
+         n_dates=3,
+         n_sites=3,
+         n_treatments=3,
+         n_loci=10e3,
+         save_data_tables=TRUE)$list_fnames_tables
+    df = utils::read.delim(list_fnames_tables$fname_phenotypes, header=TRUE)
+    df[1,1] = paste0("'", df[1,1], "'")
+    df[2,2] = paste0('"', df[2,2], '"')
+    df[3,3] = paste0(df[3,3], "\n")
+    df[4,4] = paste0(df[4,4], "\r\n")
+    df = fn_remove_quotes_and_newline_characters_in_data(df=df, verbose=TRUE)
+    expect_equal(grepl("'", df[1,1]), FALSE)
+    expect_equal(grepl('"', df[2,2]), FALSE)
+    expect_equal(grepl("\n", df[3,3]), FALSE)
+    expect_equal(grepl("\r\n", df[4,4]), FALSE)
+})
+
