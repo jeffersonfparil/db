df618bceb4c5e5ef56a5e1cb4cabaa9807df34c8
jeffersonfparil <jeffersonparil@gmail.com>
2024-08-02 17:51:30 +1000

    finish import.R documentation and unit tests

---
+++ tests/testthat/test-export.R
@@ -0,0 +1,18 @@
+# library(testthat)
+# source("R/export.R")
+
+test_that("fn_check_export_inputs", {
+    set.seed(123)
+})
+
+test_that("fn_query_and_left_join_tables", {
+    set.seed(123)
+})
+
+test_that("fn_assess_df_subsets", {
+    set.seed(123)
+})
+
+test_that("fn_deserialise_genotype_data", {
+    set.seed(123)
+})
